10ca24552f049eef3e88f6bdb519f20f3f603404
purvi15t <pgupta1598@gmail.com>
2020-10-25 12:10:48 -0500

    Initial Commit

---
+++ Parstagram1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram1
+//
+//  Created by purvi gupta on 10/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
